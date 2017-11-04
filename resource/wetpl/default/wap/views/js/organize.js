75bb2c99e1229f90a13e3838f2322278fb5336ec
Xweihaifeng <Xweihaifeng@163.com>
2017-11-04 13:48:59 +0800

    + add org detail

--- resource/wetpl/default/wap/views/js/organize.js
+++ resource/wetpl/default/wap/views/js/organize.js
@@ -8,7 +8,7 @@ $(function() {
     if(!pages) {
         window.localStorage.setItem("pageNum", 1);
     }
-    var get_weid, get_parse, get_param, li_name, total, limit = 10, pageNum = 1;
+    var get_weid, get_parse, li_name, total, limit = 10, pageNum = 1;
     var pathname = window.location.pathname.split('/').slice(1,3);
     // var ApiMaterPlatQiniuDomain = 'http://oty3r3tmi.bkt.clouddn.com/';
 
@@ -38,6 +38,32 @@ $(function() {
         console.error(error);
     });
 
+    // 组织内容部分显示
+    var news_detail = function(result) {
+        document.title = result.title;
+        var thumb_image = result.thumb_image;
+        if (thumb_image.indexOf('http') != 0 && thumb_image != "") {
+            thumb_image = ApiMaterPlatQiniuDomain + thumb_image;
+        }
+        if(result.is_original == 1) {
+            result.is_original = "原创";
+        }
+        var template = `
+            <p class="rich_media_title" id="activity-name">` + result.title + `</p>
+            <div class="clearfix bts">
+                <span class="publisher">` + result.publisher.substr(0, 5) + `</span>
+                <span class="author">` + result.auth.substr(0, 5) + `</span>
+                <span class="source_url">`+ result.source_url +`</span>
+                <span class=updated_at">` + result.publish_time.substr(0, 10) + `</span>
+                <span class="original">` + result.is_original + `</span>
+            </div>
+            <div class="page-info"><div class="page-details"><p>` + result.content + `</p></div></div>
+            <div class="evaluate"><p class="readings"><a class="read_original" href="`+ result.source_url +`" target="_blank"> 阅读原文 </a><span>阅读 ` + result.views + `</span><span class="zan" id="zan"><i class="gesture"></i><span class="zanCount">` + result.praise_num + `<span></span></p></div>`
+
+        return template;
+    }
+
+
     if(pathname.length == 2 && pathname[1] != '') {
         get_parse = pathname[1];
         var options2 = $.get(CMS_DETAIL + get_parse);
@@ -45,11 +71,14 @@ $(function() {
 
             document.title = body.data.title;
             if(body.code === 200) {
-                get_param = body.data.cate_id;
-                if(body.data != null) {
-                    $(".paging").hide();
-                    $(".list-article-ul").html(`<div class="org_content"><h3>` + body.data.summary + `</h3><div>` + body.data.content + `</div></div>`);
+                var result = body.data;
+                if(!result) {
+                    $(".list-article-ul").html(news_detail(result););
                 }
+                // if(body.data != null) {
+                //     $(".paging").hide();
+                //     $(".list-article-ul").html(`<div class="org_content"><h3>` + body.data.summary + `</h3><div>` + body.data.content + `</div></div>`);
+                // }
 
             } else {
                 console.warn(body.message);
