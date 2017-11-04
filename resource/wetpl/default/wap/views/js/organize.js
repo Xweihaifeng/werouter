fb6ad485d87341389b3e52be00fbade1bd9c3f03
Xweihaifeng <Xweihaifeng@163.com>
2017-11-04 14:12:13 +0800

    * monify organized

--- resource/wetpl/default/wap/views/js/organize.js
+++ resource/wetpl/default/wap/views/js/organize.js
@@ -8,35 +8,10 @@ $(function() {
     if(!pages) {
         window.localStorage.setItem("pageNum", 1);
     }
-    var get_weid, get_parse, li_name, total, limit = 10, pageNum = 1;
+    var get_weid, get_parse;
     var pathname = window.location.pathname.split('/').slice(1,3);
-    // var ApiMaterPlatQiniuDomain = 'http://oty3r3tmi.bkt.clouddn.com/';
 
     get_weid = pathname[0];
-/*    var options1 = $.get(CMS_CHANNELS_DOMAIN_QUERY + get_weid);
-    options1.done(function(data) {
-        if(data.code == -200) {
-            console.info(data.message);
-            return false;
-        }
-        if(data.code === 200) {
-            var thumb_image = data.data.thumb_image;
-            if (thumb_image.indexOf('http') != 0 && thumb_image != "") {
-                thumb_image = ApiMaterPlatQiniuDomain + thumb_image;
-                // thumb_image = imgSet(thumb_image, 1100, 235, 3);
-            }
-            if(thumb_image != ""){
-                $("#thumb_image").attr("src", thumb_image);
-            }
-            console.log(data);
-            get_weid = data.data.weid;
-        } else {
-            console.error(data.message);
-        }
-    });
-    options1.fail(function(error) {
-        console.error(error);
-    });*/
 
     // 组织内容部分显示
     var news_detail = function(result) {
@@ -48,18 +23,18 @@ $(function() {
         if(result.is_original == 1) {
             result.is_original = "原创";
         }
-        var template = `
-            <p class="rich_media_title" id="activity-name">` + result.title + `</p>
-            <div class="clearfix bts">
+            /*<div class="clearfix bts">
                 <span class="publisher">` + result.publisher.substr(0, 5) + `</span>
                 <span class="author">` + result.auth.substr(0, 5) + `</span>
                 <span class="source_url">`+ result.source_url +`</span>
                 <span class=updated_at">` + result.publish_time.substr(0, 10) + `</span>
                 <span class="original">` + result.is_original + `</span>
-            </div>
+            </div>*/
+        var template = `
+            <p class="rich_media_title" id="activity-name">` + result.title + `</p>
             <div class="page-info"><div class="page-details"><p>` + result.content + `</p></div></div>
-            <div class="evaluate"><p class="readings"><a class="read_original" href="`+ result.source_url +`" target="_blank"> 阅读原文 </a><span>阅读 ` + result.views + `</span><span class="zan" id="zan"><i class="gesture"></i><span class="zanCount">` + result.praise_num + `<span></span></p></div>`
-
+            `
+            // <div class="evaluate"><p class="readings"><a class="read_original" href="`+ result.source_url +`" target="_blank"> 阅读原文 </a><span>阅读 ` + result.views + `</span><span class="zan" id="zan"><i class="gesture"></i><span class="zanCount">` + result.praise_num + `<span></span></p></div>
         return template;
     }
 
