d2a22652ae55c38905ae6cb4a7f136726d0b2e39
zhuhongguang <68053846@qq.com>
2017-11-23 15:12:55 +0800

    update

--- resource/wetpl/default/wap/wepage/interface/my-article.js
+++ resource/wetpl/default/wap/wepage/interface/my-article.js
@@ -41,3 +41,29 @@ let domain = window.location.pathname.split('/')[1];
 reqUserId('http://new.wezchina.com/api/pages/page/getDetailByDomain/', domain);
 
 const my_article = resp;
+
+
+/*let resp;
+const reqDynamicList = (url) => {
+	$.ajax({
+		url: url,
+		type: 'GET',
+		async: false,
+		success: function(data){
+			if (data.code == 200) {
+				resp = data.data.list;
+				console.log(resp)
+			}
+		},
+		error: function(xhr) {
+			console.log(xhr);
+		}
+	})
+}
+
+let type = 1;
+let domain = window.location.pathname.split('/')[1];
+let dynamic_type = 1;
+reqDynamicList('http://new.wezchina.com/api/circel/dynamic?type=' + type + '&domain=' + domain + '&dynamic_type=' + dynamic_type);
+
+const my_article = resp;*/
