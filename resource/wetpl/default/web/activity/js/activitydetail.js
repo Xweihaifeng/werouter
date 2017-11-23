53db985fb6f33f96887590a79becceae8d523496
zhushuha <11276740@qq.com>
2017-11-23 16:24:09 +0800

    no message

--- resource/wetpl/default/web/activity/js/activitydetail.js
+++ resource/wetpl/default/web/activity/js/activitydetail.js
@@ -421,7 +421,7 @@ $(document).ready(function() {
                     '<div class="clon1"></div>' +
                     '<div class="sign_head_contain">' +
                     '<div class="act_title">' +
-                    (rep.data.type == 1 ? '<img src="\/common\/img\/ticket-jia.png" ></div>' : `<p>￥<b>`
+                    (rep.data.type == 1 ? '<img src="\/common\/img\/ticket-jia.png" ></div>' : '<p>￥<b>' +
                         rep.data.price `</b></p>`) +
                     '</div>' +
                     '</div>' +
