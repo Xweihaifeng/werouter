ed3a007500daecfd687c492193b25730e348b637
zhushuha <11276740@qq.com>
2017-11-23 20:38:34 +0800

    no message

--- resource/wetpl/default/web/user/js/activitycommon.js
+++ resource/wetpl/default/web/user/js/activitycommon.js
@@ -1008,7 +1008,7 @@ $(function() {
     var formObj = {
         init: function(node) {
             data = this.formatData($(node).data('status'));
-            console.log(data);
+            console.log(data + '---------------------------------------------------------');
             return this.validate();
         },
         validate: function() {
