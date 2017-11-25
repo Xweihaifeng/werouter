992b3b2731b0827dd923f8cd77bc18867227e2af
Xweihaifeng <Xweihaifeng@163.com>
2017-11-25 15:27:39 +0800

    *monify

--- resource/wetpl/default/wap/interface/my-shopping.js
+++ resource/wetpl/default/wap/interface/my-shopping.js
@@ -67,3 +67,50 @@ const my_wemall = {
         }
     ]
 }
+
+
+
+
+let resp;
+const reqArtList = (url, userid) => {
+    $.ajax({
+        url: url + userid,
+        type: 'GET',
+        async: false,
+        success: function(data){
+            if (data.code == 200) {
+                //console.log(data)
+                resp = data;
+            }
+        },
+        error: function(xhr) {
+            console.log(xhr);
+        }
+    })
+}
+
+const reqUserId = (url, domain) => {
+    $.ajax({
+        url: url + domain,
+        type: 'GET',
+        async: false,
+        success: function(data) {
+            if (data.code == 200) {
+                //console.log(data)
+                let userid = data.data.plat_user_id;
+                let info = {
+                    user_id: userid
+                }
+                reqArtList(ARTICLE_LIST + '?userId=', userid);
+            }
+        },
+        error: function(xhr) {
+            console.log(xhr);
+        }
+    })
+}
+
+let domain = window.location.pathname.split('/')[1];
+reqUserId('http://new.wezchina.com/api/pages/page/getDetailByDomain/', domain);
+
+const my_wemall = resp;
