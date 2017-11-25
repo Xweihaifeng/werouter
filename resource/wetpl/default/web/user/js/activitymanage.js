759ca607331e4cab8ca35e2a2081d87f52c266e3
zhushuha <11276740@qq.com>
2017-11-25 15:31:01 +0800

    no message

--- resource/wetpl/default/web/user/js/activitymanage.js
+++ resource/wetpl/default/web/user/js/activitymanage.js
@@ -348,6 +348,7 @@
              btn_cancle_pub +
              '<!--进行中或已结束活动都可查看报名签到管理-->' +
              '<a href="/user/admin/activity/apply/' + data.weid + '" class="btn btn-active">报名管理</a>' +
+             '<a href="/user/admin/activity/apply/' + data.weid + '" class="btn btn-active">签到列表</a>' +
              '</div>' +
              '</div>' +
              '<div class="operation">' +
