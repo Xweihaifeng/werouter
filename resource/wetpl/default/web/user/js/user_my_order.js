ad2d33aaf475deb262883099c20b488004827540
Xweihaifeng <Xweihaifeng@163.com>
2017-11-29 16:03:58 +0800

    * monify

--- resource/wetpl/default/web/user/js/user_my_order.js
+++ resource/wetpl/default/web/user/js/user_my_order.js
@@ -11,44 +11,95 @@ var domain,
 if(token) {
     $.ajaxSetup({
         global: true,
+        async:  false,
         headers: {
             'Token': token,
         }
     });
 }
 
+var order_goods = function(key, index) {
+	var thumb_image = key.goods_cover;
+    if(!thumb_image) {
+        thumb_image = "/common/img/news_default.jpg";
+    } else if (thumb_image.indexOf('http') != 0 && thumb_image != "") {
+        thumb_image = imgSet(thumb_image, 80, 80, 3);
+    }
+
+	var template = `
+	<div class="my_order_goods">
+		<div class="my_order_goods_cover"><img src="`+ thumb_image +`" alt=""></div>
+		<div class="my_order_goods_title">`+ key.goods_title +`</div>
+		<div class="my_order_order_price">￥`+ key.goods_price +`</div>
+		<div class="my_order_goods_num">`+ key.goods_num +`</div>
+	</div>`
+	return template;
+}
+
 var orderlist = function(key, index) {
+	// 	<div class="my_order_store_name"><span>`+ key.username +`</span><span>`+ key.phone +`</span></div>
+	// <div class="my_order_store_address">`+ key.address_detail +`</div>
+	
+	var order_status;
+
+	switch(key.order_status) {
+	    case 1:
+	        order_status = "已下单";
+	        break;
+	    case 2:
+	        order_status = "已付款";
+	        break;
+	    case 3:
+	        order_status = "已发货";
+	        break;
+	    case 4:
+	        order_status = "确认收货";
+	        break;
+	    case 5:
+	        order_status = "待评价";
+	        break;
+	    case 6:
+	        order_status = "取消订单";
+	        break;
+	    case 7:
+	        order_status = "确定交易";
+	        break;
+	    case 8:
+	        order_status = "退款申请 ";
+	        break;
+	    case 9:
+	        order_status = "确认退款";
+	        break;
+	    case 10:
+	        order_status = "已删除";
+	        break;
+	}
+
 	var template = `
 	<div class="my_order_store">
 		<div class="my_order_store_title">
-			<div class="my_order_store_title_time"> 2017-11-28 </div>订单号：
-			<div class="my_order_store_number"> 20171128156161685 </div>
-			<div class="my_order_store_name"><span>好奇害死猫</span><span>15804564654</span></div>
-			<div class="my_order_store_address">北京市朝阳区三里屯永茂大厦华语间</div>
-			<div class="my_order_store_clear">删除</div>
+			<div class="my_order_store_title_time">`+ key.updated_at.substr(0, 10) +`</div>订单号：
+			<div class="my_order_store_number">`+ key.order_num +`</div>
+			<div class="my_order_store_status">`+ order_status +`</div>
+			<div class="my_order_store_clear" id="`+ key.weid +`">删除</div>
 		</div>
 		<ul class="my_order_store_list">
-			<li class="my_order_img_info">
-				<div class="my_order_goods">
-					<div class="my_order_goods_cover"><img src="/common/img/avatar6.png" alt=""></div>
-					<div class="my_order_goods_title">【天猫超市】黑人牙膏超白竹炭倍健190g*4口腔清洁家庭套装清新 </div>
-					<div class="my_order_order_price">￥1564</div>
-					<div class="my_order_goods_num">1561346</div>
-				</div>
-				<div class="my_order_goods">
-					<div class="my_order_goods_cover"><img src="/common/img/avatar6.png" alt=""></div>
-					<div class="my_order_goods_title">【天猫超市】黑人牙膏超白竹炭倍健190g*4口腔清洁家庭套装清新 </div>
-					<div class="my_order_order_price">￥1564</div>
-					<div class="my_order_goods_num">1561346</div>
-				</div>
+			<li class="my_order_img_info">`
+
+				$.map(key.goods, function(value, i) {
+					template += order_goods(value, i);
+				})
+
+				template +=`
+
 			</li>
 			<li class="my_order_price">
 				<div class="my_order_order_price">￥1564<div> (含运费：0.00) </div></div>
 			</li>
 			<li class="my_order_operation">
 				<div class="my_order_order_operation">
-					<a href="">订单详情</a>
-					<a href="">待发货</a>
+					<a href="/user/order/detail/`+ key.weid +`" target="_blank">订单详情</a>
+					<a href="javascript:void(0)">待发货</a>
 				</div>
 			</li>
 		</ul>
@@ -81,24 +132,12 @@ var orderlisthtml = function(plat_user_id) {
     });
 }
 
-function mall_userdetail() {
-    var options2 = $.get(apiUrl + "mall/userdetail");
-    options2.done(function(data) {
-        if(data.code == 200) {
-            console.log(data);
-        }
-    });
-    options2.fail(function(error) {
-        console.error(error);
-    });
-}
-
 var hasDomain = function(weid) {
     // Pages - 主页 - 详情(用户ID)
     var options1 = $.get(apiUrl + "pages/page/getDetailByUser/" + weid);
     options1.done(function(data) {
         if(data.code == 200) {
-            console.log("主页详情", data.data);
+            // console.log("主页详情", data.data);
             if(!data.data) {
                 weid = data.data.plat_user_id;
                 domain = "/" + data.data.domain + "/";
