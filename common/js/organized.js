d625e29014aa55b36bfd79bdafd1d7b6e157e70f
Xweihaifeng <Xweihaifeng@163.com>
2017-12-06 10:36:19 +0800

    * monify

--- common/js/organized.js
+++ common/js/organized.js
@@ -4,7 +4,15 @@
 
 $(function() {
 
+    var token = window.localStorage.getItem('token');
     var channels = window.location.pathname.split('/').slice(1,3);
+    $.ajaxSetup({
+        global: true,
+        async:  false,
+        headers: {
+            'Token': token,
+        }
+    });
 
     // 查询频道
     $.ajax({
@@ -45,62 +53,6 @@ $(function() {
         }
     });
 
-    // 查询组织频道下面所有文章
-    $.ajax({
-        url: apiUrl + "/cms/contents?channel_id=org",
-        dataType: 'json',
-        success: function(data){
-            // console.log("所有频道文章", data.data);
-        },
-        error: function(xhr){
-            console.log(xhr);
-        }
-    });
-
-    // 查询组织栏目分类
-    $.ajax({
-        url: apiUrl + "/cms/channel_categories?channel=org",
-        dataType: 'json',
-        success: function(data){
-            // console.log("所有栏目");
-
-            $.map(data.data, function(item, index) {
-                if(item.title == '入会申请') {
-                    $(".dier").append(`
-                        <p class="title">`+ item.title +`</p>
-                        <ul id="ruhuishenqing" name="`+ item.weid +`"></ul>`
-                    );
-                    column_rhsq(item.weid, item.thumb_image);
-                } else if(item.title == "发展报告") {
-                    $(".diyi .title").text(item.title);
-                    report(item.weid, item.thumb_image);
-                } else if(item.title == "光彩事业") {
-                    $(".disan .title").text(item.title);
-                    lunbo(item.weid, item.thumb_image);
-                } else {
-                    if(index <= 8) {
-                        $(".shanghuijieshao").append(column_list(item));
-                    }
-                }
-            });
-
-            // 移入加载栏目分类
-            $(".list").hover(function() {
-                $(this).find(".sub").addClass("list_selected").fadeIn(100);
-                var name = $(this).attr("name");
-                $(".list_selected").html("");
-                list_name(name);
-
-            },function() {
-                $(this).find(".sub").removeClass("list_selected").fadeOut(500);
-            });
-
-        },
-        error: function(xhr){
-            console.log(xhr);
-        }
-    });
-
     // 查询栏目分类所有文章
     function list_name(name) {
         $.ajax({
@@ -120,7 +72,8 @@ $(function() {
         });
     }
 
-    var column_rhsq = function(data) {
+    // 入会申请
+    function column_rhsq(data, thumb_imgs) {
         $.ajax({
             url: apiUrl + "/cms/contents?cate_id=" + data,
             dataType: 'json',
@@ -129,7 +82,7 @@ $(function() {
                     thumb_imgs = imgSet(thumb_imgs, 45, 40, 3);
                     $(".dier #ruhuishenqing").css({"background-image": "url("+ thumb_imgs +")"});
                 }
-
+                debugger;
                 $.map(data.data.list, function(item, index) {
                     if(index <= 5) {
                         var thumb_image = item.thumb_image;
@@ -156,7 +109,8 @@ $(function() {
         });
     }
 
-    var report = function(bg_weid, thumb_imgs) {
+    // 发展报告
+    function report(bg_weid, thumb_imgs) {
         $.ajax({
             url: apiUrl + "/cms/contents?cate_id=" + bg_weid,
             dataType: 'json',
@@ -198,7 +152,8 @@ $(function() {
         })
     }
 
-    var lunbo = function(gc_weid) {
+    // 商会介绍
+    function lunbo(gc_weid, thumb_imgs) {
         $.ajax({
             url: apiUrl + "/cms/contents?cate_id=" + gc_weid,
             dataType: 'json',
@@ -240,11 +195,32 @@ $(function() {
         })
     }
 
+    // 二级分类
+    function cont_two(two_domain, result) {
+        var options12 = $.get(apiUrl + "cms/cate_categories?cate=" + result);
+        options12.done(function(body12) {
+            if(body12.code == 200 && body12.data.length > 0) {
+                $.map(body12.data, function(value, key) {
+                    if(key < 10) {
+                        $("#" + two_domain).append(`<a href="/org/`+ value.domain +`"> <i class="iconfont">&#xe61a;</i>`+ value.title +` </a>`)
+                    }
+                });
+            }
+        })
+    }
+
+    // 一级分类
     var column_list = function(data) {
         var thumb_image = data.thumb_image;
         if (thumb_image.indexOf('http') != 0 && thumb_image != "") {
             thumb_image = imgSet(thumb_image, 45, 40, 3);
         }
+
+        var template2 = function(value) {
+            template += `<a href="/org/`+ value.domain +`"> <i class="iconfont">&#xe61a;</i>`+ value.title +` </a>`
+            return template;
+        }
+
         var template = `
         <div class="list diyidier" name="`+ data.domain +`">
             <a href="/org/`+ data.domain +`">
@@ -252,16 +228,58 @@ $(function() {
                 if(thumb_image != "") {
                     template = template + `<img class="iconfont" src="`+ thumb_image +`" alt="">`;
                 }
-                template = template + `
 
+                template = template + `
+                
             </a>
-            <div class="sub">
-                <a href="/org/`+ data.weid +`">
-                    <i class="iconfont">&#xe61a;</i>`+ data.title +`
-                </a>
-            </div>
+            <div class="sub" id="`+ data.domain +`"></div>
         </div>`
         return template;
     };
+
+    // 查询组织栏目分类
+    $.ajax({
+        url: apiUrl + "/cms/channel_categories?channel=org",
+        dataType: 'json',
+        success: function(data){
+            console.log("所有栏目", data);
+
+            $.map(data.data, function(item, index) {
+                if(item.title == '入会申请') {
+                    $(".dier").append(`
+                        <p class="title">`+ item.title +`</p>
+                        <ul id="ruhuishenqing" name="`+ item.weid +`"></ul>`
+                    );
+                    column_rhsq(item.weid, item.thumb_image);
+                } else if(item.title == "发展报告") {
+                    $(".diyi .title").text(item.title);
+                    report(item.weid, item.thumb_image);
+                } else if(item.title == "商会介绍") {
+                    $(".disan .title").text(item.title);
+                    lunbo(item.weid, item.thumb_image);
+                } else {
+                    // if(index <= 8) {
+                        $(".shanghuijieshao").append(column_list(item));
+                        cont_two(item.domain, item.weid);
+                    // }
+                }
+            });
+
+            // 移入加载栏目分类
+            $(".list").hover(function() {
+                $(this).find(".sub").addClass("list_selected").fadeIn(100);
+                var name = $(this).attr("name");
+                // $(".list_selected").html("");
+                // list_name(name);
+
+            },function() {
+                $(this).find(".sub").removeClass("list_selected").fadeOut(500);
+            });
+
+        },
+        error: function(xhr){
+            console.log(xhr);
+        }
+    });
 
 });
