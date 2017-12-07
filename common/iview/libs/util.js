f975980b1a3154b1e001399229fce1c1c226a0d7
HuangYin <kuyuechaosheng@163.com>
2017-12-07 15:02:16 +0800

    *

---
+++ common/iview/libs/util.js
@@ -0,0 +1,37 @@
+var ajax = {};
+var apiUrl_url = 'https://wezchina.com/api'
+ajax = axios.create({
+    baseURL: apiUrl_url,
+    timeout: 30000,
+});
+
+// http request 拦截器
+ajax.interceptors.request.use(
+    config => {
+        config.headers.Token = localStorage.getItem('token');
+        return config;
+    }
+);
+
+ajax.interceptors.response.use(
+    response => {
+        switch (response.data.code){
+            case 200:
+                if (response.data.message == 'Success'){
+                    return response.data;
+                }
+                else
+                {
+                    return false;
+                }
+                break;
+            case -200:
+                return response.data;
+                break;
+            case 401:
+                window.location.href = '/login?url='+window.location.pathname;
+                break;
+        }
+    }
+);
+
---
+++ common/public/public.js
@@ -0,0 +1,15 @@
+// 临时放到这里
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  unescape(r[2]); return null;
+}
+
+function empty(params)
+{
+    if(params !== null && params !== undefined && params !== '')
+    {
+        return true;
+    }
+    return false;
+}
