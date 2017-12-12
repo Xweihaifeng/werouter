d4466391d25097753b327e1ebc6854dcca5ad202
zhangliping <2215161439@qq.com>
2017-12-12 12:02:01 +0800

    李生

--- resource/wetpl/default/web/user/js/wechat-update.js
+++ resource/wetpl/default/web/user/js/wechat-update.js
@@ -53,7 +53,7 @@ $(document).ready(function(){
 		$("#myModal").removeClass("in").hide();
 		$(".modal-backdrop.fade.in").remove();
 	}
-    
+    /*
     $("#save_setup").click(function() {
     	$("#myModal").show();
     	var body107 = new Object();
@@ -78,14 +78,13 @@ $(document).ready(function(){
             return false;
         }
     	$("#wechat_save").show();    	
-    	$(".modal_info").html("本次提交为重要信息,请仔细检查确无误后再提交（如果有误平台支付模块将报错此信息必须和微信公众平台，商户平台信息保存一致并且是可用状态）请确认后谨慎操作！").css("color", "");    	
-
-    	$("#wechat_save").click(function() {
+    	$(".modal_info").html("本次提交为重要信息,请仔细检查确无误后再提交（如果有误平台支付模块将报错此信息必须和微信公众平台，商户平台信息保存一致并且是可用状态）请确认后谨慎操作！").css("color", "#f00");
+        $("#wechat_save").click(function() {
             $('#uploadForm').submit();
         });
 	    
     });
-
+    */
 	$(".close_modal").click(function() {
     	clear();
     });
@@ -103,24 +102,122 @@ $(document).ready(function(){
         $("input[name=verify_file_name]").val(file.name);
         $("#filename").html(file.name);
     });
-})
+    $("#wechat_open_save").click(function () {
+        var body107 = new Object();
+        body107.weid            = get_weid;
+        body107.app_id 			= $("#app_id")		 	.val();
+        body107.app_cert 		= $("#app_cert")	    .val();
+        if(!body107.app_id){
+            layer.msg("公众号id为空！", {time: 5000});
+            return false;
+        }
+        if(!body107.app_cert){
+            layer.msg("公众号秘钥为空！", {time: 5000});
+            return false;
+        }
+        addWechatPost(new FormData($("#uploadForm")[0]));
+    });
 
-function addWechatPost() {
-    $.ajax({
-        type: "POST",
-        async: false,
-        cache: false,
-        contentType: false,
-        processData: false,
-        url: apiUrl + "pages/wechat/update",
-        data:new FormData($( "#uploadForm" )[0]),// 要提交表单的ID
-        success: function (data) {
-            if (data.code == 200) {
-                layer.msg("修改成功！", {time: 1500});
-            } else {
 
-            }
+    $('#open').click(function(){
+        var text = $(this).text();
+        if(text === "展开"){
+            $('.open_content0').stop().slideDown(400);
+            $(this).text("收起");
+        }else {
+            $('.open_content0').stop().slideUp(400);
+            $(this).text("展开");
+
         }
     });
-    return false;
-}
+    $('#open1').click(function(){
+        var text = $(this).text();
+        if(text === "展开"){
+            $('.open_content1').stop().slideDown(400);
+            $(this).text("收起");
+        }else {
+            $('.open_content1').stop().slideUp(400);
+            $(this).text("展开");
+
+        }
+    });
+    $('#open2').click(function(){
+        var text = $(this).text();
+        if(text === "展开"){
+            $('.open_content2').stop().slideDown(400);
+            $(this).text("收起");
+        }else {
+            $('.open_content2').stop().slideUp(400);
+            $(this).text("展开");
+
+        }
+    });
+    $('#open3').click(function(){
+        var text = $(this).text();
+        if(text === "展开"){
+            $('.open_content3').stop().slideDown(400);
+            $(this).text("收起");
+        }else {
+            $('.open_content3').stop().slideUp(400);
+            $(this).text("展开");
+
+        }
+    });
+
+    $("#wechat_merchant_save").click(function () {
+        var body107 = new Object();
+        body107.weid            = get_weid;
+        body107.merchant_id 	= $("#merchant_id")	 	.val();
+        body107.merchant_key 	= $("#merchant_key")	.val();
+        body107.apiclient_cert 	= $("#apiclient_cert") 	.val();
+        body107.apiclient_key 	= $("#apiclient_key")	.val();
+        if(!body107.merchant_id){
+            layer.msg("商户号ID为空！", {time: 5000});
+            return false;
+        }
+        if(!body107.merchant_key){
+            layer.msg("商户号密钥为空！", {time: 5000});
+            return false;
+        }
+        if(!body107.apiclient_cert){
+            layer.msg("api证书为空！", {time: 5000});
+            return false;
+        }
+        if(!body107.apiclient_key){
+            layer.msg("api证书密钥为空！", {time: 5000});
+            return false;
+        }
+        addWechatPost(new FormData($("#uploadForm1")[0]));
+    });
+    $("#wechat_verify_save").click(function () {
+        var body107 = new Object();
+        body107.weid            = get_weid;
+        body107.verify_file_name 	= $("input[name=verify_file_name]")	.val();
+        if(!body107.verify_file_name){
+            layer.msg("商户号ID为空！", {time: 5000});
+            return false;
+        }
+        addWechatPost(new FormData($("#uploadForm2")[0]));
+    });
+
+    //提交数据
+    function addWechatPost(data) {
+        $.ajax({
+            type: "POST",
+            async: false,
+            cache: false,
+            contentType: false,
+            processData: false,
+            url: apiUrl + "pages/wechat/update",
+            data:data,// 要提交表单的ID
+            success: function (data) {
+                if (data.code == 200) {
+                    layer.msg("修改成功！", {time: 1500});
+                } else {
+                    layer.msg(data.message, {time: 1500});
+                }
+            }
+        });
+    }
+
+})
