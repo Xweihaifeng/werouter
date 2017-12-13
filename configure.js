f0b2f423b572db0b5d37af1698a33d6880f0066c
HuangYin <kuyuechaosheng@163.com>
2017-12-13 17:08:13 +0800

    *

--- configure.js
+++ configure.js
@@ -4,8 +4,9 @@
 
 //接口配置
 const httpType = window.location.protocol;
-// const apiUrl = httpType + '//{{url}}/api/';
-const apiUrl = httpType + '//new.wezchina.com/api/';
+
+const apiUrl = httpType + '//{{url}}/api/';
+
 const siteUrl = httpType + '//{{url}}';
 
 const domainHost = window.location.host;
