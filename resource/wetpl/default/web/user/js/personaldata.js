37a10ab4bcd5bc9d7ffb1ea2ae1a5daab366148b
zhushuha <11276740@qq.com>
2017-12-14 14:47:35 +0800

    no message

--- resource/wetpl/default/web/user/js/personaldata.js
+++ resource/wetpl/default/web/user/js/personaldata.js
@@ -512,10 +512,10 @@ $(document).ready(function() {
             title: '微信绑定',
             offset: type //具体配置参考：http://www.layui.com/doc/modules/layer.html#offset
                 ,
-            area: ['400px', '500px'],
+            area: ['220px', '350px'],
             id: 'layerDemo' + type //防止重复弹出
                 ,
-            content: '<div id="qrcode-block"  style="padding: 20px 100px;"></div>'
+            content: '<div id="qrcode-block"></div>'
                 //,btn: '关闭全部'
                 ,
             btnAlign: 'c' //按钮居中
