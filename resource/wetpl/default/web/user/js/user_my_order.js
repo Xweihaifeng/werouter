4667ad9c48437ecb26d0ac71aa325e772022157c
zhangliping <2215161439@qq.com>
2017-12-15 16:11:13 +0800

    李生

--- resource/wetpl/default/web/user/js/user_my_order.js
+++ resource/wetpl/default/web/user/js/user_my_order.js
@@ -745,6 +745,13 @@ $(function () {
                         var obj=$("#"+orderid).find(".status-oper");
                         obj.empty();
                         obj.append(operDom);
+                        //重新加载页面
+                        var time_start=$("input[name='start_time']").val();
+                        var time_end=$("input[name='end_time']").val();
+                        var order_num=$("input[name='no']").val();
+                        var statusid=$(".current .btn-select").data("id");
+                        var page=$("pagination").find("active").find("span").text();
+                        orderlist(userId,page,statusid,{time_start:time_start,time_end:time_end,order_num:order_num});
                     }
                 }
             }
