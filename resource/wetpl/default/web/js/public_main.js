931a4b24bbdfac3e6823133bdab0f10f500d7dd8
zhuhongguang <68053846@qq.com>
2017-12-16 10:09:21 +0800

    update

--- resource/wetpl/default/web/js/public_main.js
+++ resource/wetpl/default/web/js/public_main.js
@@ -1348,7 +1348,7 @@ req().then((data) => {
                 }, 1000)*/
 
                 genEvt();
-                org();
+                //org();
                 //help();
                 //share();
 
