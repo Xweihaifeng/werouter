a8708f11386d7d54a10b67a51cc91de56d99ce82
guanzhao8051 <guanzhao@vip.qq.com>
2017-12-18 13:39:44 +0800

    更改微主页标题显示

--- controller/mp.php
+++ controller/mp.php
@@ -3,7 +3,6 @@ class mp extends controller
 {
 	public function index()
 	{
-
 		$protocol = empty($_SERVER['HTTPS']) ? 'http://' : 'https://';
 		echo file_get_contents( $protocol.$_SERVER['HTTP_HOST'].'/api'.$_SERVER['REQUEST_URI']);
 		exit();
