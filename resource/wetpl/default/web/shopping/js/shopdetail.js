690ba099583bb86983a222141fdb02311dc597af
zhangliping <2215161439@qq.com>
2017-12-19 22:15:50 +0800

    李生

--- resource/wetpl/default/web/shopping/js/shopdetail.js
+++ resource/wetpl/default/web/shopping/js/shopdetail.js
@@ -337,7 +337,16 @@ $(document).ready(function() {
                         'padding':'0px',
                         'hideOnOverlayClick':false,
                         'closeBtn':false,
-                        'autoScale'   : true
+                        'autoScale'   : true,
+                        'tpl':{
+                            'wrap'     : '<div class="fancybox-wrap" tabIndex="-1"><div class="fancybox-skin"><div class="fancybox-outer"><div class="fancybox-inner"></div></div></div></div>',
+                            'image'    : '<img class="fancybox-image" src="{href}" alt="" />',
+                        	'iframe'   : '<iframe id="fancybox-frame{rnd}" name="fancybox-frame{rnd}" class="fancybox-iframe" frameborder="0" vspace="0" hspace="0"' + ($.browser.msie ? ' allowtransparency="true"' : '') + '></iframe>',
+                        	'error'    : '<p class="fancybox-error">The requested content cannot be loaded.<br/>Please try again later.</p>',
+                    		'closeBtn' : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"></a>',
+                    		'next'     : '<a title="下一个" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
+                    		'prev'     : '<a title="上一个" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
+                        	}
 					});
 				} else {
 					layer.msg("MESSAGE IS ERROR", { time: 2500 });
