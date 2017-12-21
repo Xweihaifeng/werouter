96495280e1a55012a9d346c5eea22391693ab674
G_lin <alertlin@163.com>
2017-12-21 11:29:48 +0800

    '修改动态公共接口'

--- common/public/js/mobile_common_ajax.js
+++ common/public/js/mobile_common_ajax.js
@@ -26,7 +26,7 @@
 	//获取动态数据  type = 1 是我的动态  2是我关注人的动态
 	// dynamic_type 1文章2项目3活动4商城
 	mob_ajax.dynamic  = function(type , dynamic_type , call){
-		ajax.post('/circel/dynamic' , params :{{type: type ,  dynamic_type : dynamic_type ,domain: domain}}).then((res)=>{
+		ajax.get('/circel/dynamic' , {params :{type: type ,  dynamic_type : dynamic_type}}).then((res)=>{
 			call(res);
 		});
 	}
