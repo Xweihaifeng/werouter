b49fde69e3ac5aa6312ea93dbe1b4f135e0abc0d
guanzhao8051 <guanzhao@vip.qq.com>
2017-12-22 09:16:32 +0800

    修改过滤器 img_src && formateTime

--- common/iview/libs/filter.js
+++ common/iview/libs/filter.js
@@ -1,5 +1,9 @@
-Vue.filter('img_src', function (value , width , height) {
+Vue.filter('img_src', function (value , width , height, mode) {
     if (!value) return '';
+
+    if(value.indexOf('http') !== -1){
+        return value;
+    }
 
     var settings = http_type + plats_qiniu.domain_custom + '/';
 
@@ -9,21 +13,13 @@ Vue.filter('img_src', function (value , width , height) {
     }
     else
     {
-        return settings + value + '?imageView2/1/w/' + width + '/h/' + height;
-    }
-});
-
-Vue.filter('imgSet', function (value, width, height, mode) {
-    if(value.indexOf('http') !== -1){
-        return value
-    }else{
         if (mode != undefined) {
-            return ApiMaterPlatQiniuDomain + value + '?imageView2/' + mode + '/w/' + width + '/h/' + height;
+            return settings + value + '?imageView2/' + mode + '/w/' + width + '/h/' + height;
         } else {
-            return ApiMaterPlatQiniuDomain + value + '?imageView2/3/w/' + width + '/h/' + height;
+            return settings + value + '?imageView2/3/w/' + width + '/h/' + height;
         }
     }
-})
+});
 
 Vue.filter('href', function (value) {
     if (!value) return '';
@@ -32,7 +28,8 @@ Vue.filter('href', function (value) {
 });
 
 Vue.filter('formateTime',function (value,type) {
-    var date = new Date(value);
+
+    var date = new Date(value * 1000);
     var year = date.getFullYear();
     var month = (date.getMonth() + 1).toString().length == 1 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
     var day = (date.getDay() + 1).toString().length == 1 ? '0' + (date.getDay() + 1) : date.getDay() + 1;
@@ -40,6 +37,9 @@ Vue.filter('formateTime',function (value,type) {
     var min = date.getMinutes().toString().length == 1 ? '0' + date.getMinutes() : date.getMinutes();
     if(type == 'MDHM'){
         return month +'-'+day+' '+hour+':'+min
+    }
+    else if(type == 'YMDHM'){
+        return year + '-' +month +'-'+day+' '+hour+':'+min
     }else if(type == 'HM'){
         return hour+':'+min;
     }
