cbade35a3049c1c4de377163716a3a920c31ad88
HuangYin <kuyuechaosheng@163.com>
2017-12-22 15:27:13 +0800

    *

--- admin/js/template/settings.js
+++ admin/js/template/settings.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
                     $('#domain').val(data.data.domain);
                     $('#plat_name').val(data.data.plat_name);
                     $("input[name=http_type][value=" + data.data.http_type +  "]").attr('checked', true);
+                    $("input[name=wap_domain][value=" + data.data.wap_domain +  "]").attr('checked', true);
                 } else {
                     console.log('error: -200');
                 }
@@ -26,11 +27,12 @@ $(document).ready(function () {
     $("#updateSet").click(function () {
         var plat_name = $('#plat_name').val();
         var http_type = $("input[name='http_type']:checked").val();
+        var wap_domain = $("input[name='wap_domain']:checked").val();
         $.ajax({
             url: ApiUrl + 'plat_setting',
             type: 'post',
             dataType: 'json',
-            data: {plat_name: plat_name, http_type: http_type},
+            data: {plat_name: plat_name, http_type: http_type, wap_domain:wap_domain},
             success: function (data) {
                 if (data.code === 200) {
                     swal('提示', '保存成功', 'success');
