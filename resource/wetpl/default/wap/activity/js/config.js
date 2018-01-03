610f9766d052abf10576ee7ac1eb65b599dde9b6
zhuhongguang <68053846@qq.com>
2018-01-03 18:13:49 +0800

    act

---
+++ resource/wetpl/default/wap/activity/js/config.js
@@ -0,0 +1,59 @@
+var url = window.location.href,
+    host = 'https://g.welian.com/',
+    version = 0;
+
+var getPath = function() {
+    var js = document.scripts,
+        script = js[js.length - 1],
+        jsPath = script.src;
+    if (script.getAttribute('merge')) return;
+    return jsPath.substring(0, jsPath.lastIndexOf("/") + 1);
+}
+
+if (url.indexOf('http://h5.welian.com') === -1) {
+    host = 'http://' + getPath().split('/')[2];
+}
+
+$script = document.getElementsByTagName('script');
+
+for (var i = 0; i < $script.length; i++) {
+    if ($script[i].src.indexOf('config.js')) {
+        version = $script[i].src.replace('http://').split('/')[1];
+    }
+}
+
+//var base_url = host;
+var base_url = {{PATH_TML}} + 'activity/js/';
+seajs.config({
+    alias: {
+        jweixin: 'https://res.wx.qq.com/open/js/jweixin-1.2.0.js',
+        wx: base_url + '/h5/common/wx.js',
+        browser: base_url + '/h5/plugins/browser.js',
+        iscroll: base_url + '/common/plugins/iscroll.js',
+        welianJsBridge: base_url + '/h5/common/WelianJSBridge.js',
+        jquery: base_url + '/static/js/jquery.min.js',
+        vue: base_url + '/static/js/vue-2.1.10.min.js',
+        vueRouter: base_url + '/static/js/vue-router-2.2.1.min.js',
+        vueInfiniteLoading: base_url + '/h5/js/vue-infinite-loading.js',
+        toast: base_url + '/h5/js/toast.js',
+        vueComponents: base_url + '/h5/js/components.js'
+    },
+
+    paths: {},
+
+    vars: {
+        imageUrl: 'http://fed.welian.com'
+    },
+
+    map: [
+        // ['.js', '.js?v=' + new Date().getTime()]
+    ],
+
+    preload: [],
+
+    debug: 0,
+
+    base: base_url,
+
+    charset: 'utf-8'
+});
