7f045ed11336b40009108d649481dfb54ed5babb
G_lin <alertlin@163.com>
2018-01-05 18:13:55 +0800

    '文章添加链接，修改时间过滤器添加时间戳10位判断'

--- common/iview/libs/filter.js
+++ common/iview/libs/filter.js
@@ -58,6 +58,9 @@ Vue.filter('href', function (value , from) {
 Vue.filter('formateTime',function (value,type) {
 
     var date = new Date(value);
+    if(value.toString().length === 10){
+        date = new Date(value*1000);
+    }
     var year = date.getFullYear();
     var month = (date.getMonth() + 1).toString().length == 1 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
     var day = (date.getDay() + 1).toString().length == 1 ? '0' + (date.getDay() + 1) : date.getDay() + 1;
@@ -68,12 +71,8 @@ Vue.filter('formateTime',function (value,type) {
         case 'MDHM':
             return month +'-'+day+' '+hour+':'+min;
         case 'YMDHM':
-            date = new Date(value);
-            year = date.getFullYear();
             return year + '-' +month +'-'+day+' '+hour+':'+min;
         case 'YMD':
-            date = new Date(value);
-            year = date.getFullYear();
             return year + '-' +month +'-'+day;
         case 'HM':
             return hour+':'+min;
