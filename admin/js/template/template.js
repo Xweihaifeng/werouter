66dd0fb7ffe37e83e98dea4a214b82fd8ee39bca
zhangliping <2215161439@qq.com>
2018-01-09 20:53:24 +0800

    李生

--- admin/js/template/template.js
+++ admin/js/template/template.js
@@ -16,6 +16,93 @@ $(document).ready(function () {
                     $(".module_li").addClass("block");
                     $("#diy").addClass("shown").siblings().removeClass("shown");
                     $(".form-child > li:nth-of-type(2)").addClass("block").siblings().removeClass("block");
+                    //模版列表渲染
+                    var fileurl=localStorage.getItem('template_listfileurl');
+                    if(fileurl==''||fileurl==null||fileurl=="undefined"){
+                        fileurl=data.data.custom_file;
+                    }
+                    $.ajax({
+                        url: document.location.protocol+'//'+document.domain+'/admin/template.php',
+                        type: 'post',
+                        data:{operation : 'templateList',url:fileurl},
+                        dataType: 'json',
+                        success: function(data){
+                            //获取数据成功
+                            if(data.code==200){
+                                $('tbody').empty();
+                                for(var i=0;i<data.data.length;i++){
+                                    var html='';
+                                    if(data.data[i].style=='file'){
+                                        html+=`<tr class="odd gradeX" id="dr_row_`+i+`">
+                                                        <td class="myselect">
+                                                            <label class="mt-table mt-checkbox mt-checkbox-single mt-checkbox-outline">
+                                                                <input type="checkbox" class="checkboxes" name="ids[]" value="flink_list.html">
+                                                                <span></span>
+                                                            </label>
+                                                        </td>
+                                                        <td>
+                                                            <a href="javascript:void(0)" class="editfile" diyurl="`+data.data[i].url+`" >
+                                                                <img src="image/html.png" style="width:30px;margin-right:10px"> `+data.data[i].name+`</a>
+                                                        </td>
+                                                        <td style="text-align:center">`+data.data[i].size+` KB</td>
+                                                        <td><font color="red">`+data.data[i].updated+`</font></td>
+                                                        <td>
+                                                            <label><a href="javascript:void(0)" diyurl="`+data.data[i].url+`" class="btn btn-xs green editfile"> <i class="fa fa-edit"></i> 修改</a></label>
+                                                        </td>
+                                                    </tr>`;
+                                    }
+                                    if(data.data[i].style=='folder'||data.data[i].style=='back'){
+                                        if(data.data[i].url!=''){
+                                            html+=`<tr class="odd gradeX" id="dr_row_0">
+                                                        <td class="myselect">
+                                                            <label class="mt-table mt-checkbox mt-checkbox-single mt-checkbox-outline">
+                                                                <input type="checkbox" class="checkboxes" name="ids[]" value="">
+                                                                <span></span>
+                                                            </label>
+                                                        </td>
+                                                        <td>
+                                                            <a href="javascript:void(0)" class="listfile" diyurl="`+data.data[i].url+`">
+                                                                <img src="image/folder.png" style="width:30px;margin-right:10px"> `+data.data[i].name+`</a>
+                                                        </td>
+                                                        <td style="text-align:center"> -</td>
+                                                        <td></td>
+                                                        <td>
+                                                        </td>
+                                                    </tr>`;
+                                        }
+                                    }
+                                    $('tbody').append(html);
+                                }
+                                listfileclick();
+                                editfileclick();
+                            }
+                        },
+                        error: function(xhr){
+                            console.log(xhr);
+                        }
+                    });
+                    var listfileclick=function () {
+                        $(".listfile").click(function () {
+                            var url=$(this).attr('diyurl');
+                            if(url){
+                                //跳转本页记录url
+                                localStorage.setItem('template_listfileurl', url);
+                                document.location.reload();
+                            }else{
+                                document.location.reload();
+                            }
+                        })
+                    }
+                    var editfileclick=function () {
+                        $(".editfile").click(function () {
+                            var url=$(this).attr('diyurl');
+                            if(url){
+                                //跳转编辑页记录URL
+                                localStorage.setItem('template_editfileurl', url);
+                                window.location.href="/admin/template_detail.html";
+                            }
+                        })
+                    }
                 }
             } else {
                 console.log('error: -200');
