f5407d397aa8adb05e5f852783c70cf7b63373d6
HuangYin <kuyuechaosheng@163.com>
2018-01-09 18:56:10 +0800

    *

--- admin/js/template/template.js
+++ admin/js/template/template.js
@@ -7,22 +7,14 @@ $(document).ready(function () {
             dataType: 'JSON',
             success: function(result) {
                 if(result.code === 200) {
-                    var defaults = customs = '';
+                    var defaults = '';
                     $.each(result.data, function(key, val) {
-                        if (val.isCustom == 1) {
-                            customs += '<div style="width: 200px; float: left; text-align: center; margin-right: 20px; line-height: 40px;">' +
-                                '<img width="200" src="/resource/wetpl/' + val.mark + '/' + val.cover + '"></br>' +
-                                '<input type="radio" name="templateId" value=' + val.id + '>' + val.title +
-                                '<span style="padding-left: 15px;">预览</span></div>';
-                        } else {
-                            defaults += '<div style="width: 200px; float: left; text-align: center; margin-right: 20px; line-height: 40px;">' +
-                                '<img width="200" src="/resource/wetpl/' + val.mark + '/' + val.cover + '"></br>' +
-                                '<input type="radio" name="templateId" value=' + val.id + '>' + val.title +
-                                '<span style="padding-left: 15px;">预览</span></div>';
-                        }
+                        defaults += '<div style="width: 200px; float: left; text-align: center; margin-right: 20px; line-height: 40px;">' +
+                            '<img width="200" src="/resource/wetpl/' + val.mark + '/' + val.cover + '"></br>' +
+                            '<input type="radio" name="templateId" value=' + val.id + '>' + val.title +
+                            '<span style="padding-left: 15px;">预览</span></div>';
                     });
                     $("#default").html(defaults);
-                    $("#custom").html(customs);
 
                 } else {
                     parent.layer.msg(result.message);
