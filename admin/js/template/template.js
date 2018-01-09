69ea8178f018bf7216d22040956f5e606fb961a8
R20609 <479496771@qq.com>
2018-01-09 20:20:57 +0800

    *模板

--- admin/js/template/template.js
+++ admin/js/template/template.js
@@ -1,5 +1,31 @@
 $(document).ready(function () {
     start();
+    $.ajax({
+        url: ApiUrl + 'plat_setting',
+        type: 'get',
+        dataType: 'json',
+        success: function (data) {
+            if (data.code === 200) {
+                if (data.data.is_custom != 1) {
+                    $(".but").animate({left: -40},0);
+                    $(".hidde").slideDown();
+                    $(".module").removeClass("block");
+                    $(".module_li").addClass("block");
+                }else {
+                    $(".module").removeClass("block");
+                    $(".module_li").addClass("block");
+                    $("#diy").addClass("shown").siblings().removeClass("shown");
+                    $(".form-child > li:nth-of-type(2)").addClass("block").siblings().removeClass("block");
+                }
+            } else {
+                console.log('error: -200');
+            }
+
+        },
+        error: function (xhr) {
+            console.log(xhr);
+        }
+    });
     var init = function () {
         $.ajax({
             url: ApiUrl + "plat_template",
@@ -7,15 +33,42 @@ $(document).ready(function () {
             dataType: 'JSON',
             success: function(result) {
                 if(result.code === 200) {
-                    var defaults = '';
+                    var defaults = template = '';
                     $.each(result.data, function(key, val) {
                         defaults += '<div style="width: 200px; float: left; text-align: center; margin-right: 20px; line-height: 40px;">' +
                             '<img width="200" src="/resource/wetpl/' + val.mark + '/' + val.cover + '"></br>' +
                             '<input type="radio" name="templateId" value=' + val.id + '>' + val.title +
                             '<span style="padding-left: 15px;">预览</span></div>';
+                        template += '<option name="options" value=' + val.id + '>' + val.title + '</option>';
                     });
                     $("#default").html(defaults);
+                    $("#module_list").html(template);
+                    $("#custom_file").val(window.location.host);
+                    $("#custom_name").val('自定义模板');
+                    $.ajax({
+                        url: ApiUrl + 'plat_setting',
+                        type: 'get',
+                        dataType: 'json',
+                        success: function (data) {
+                            if (data.code === 200) {
+                                if (data.data.is_custom != 1) {
+                                    $("#templateId").find("input[name=templateId][value=" + data.data.template_id + "]").attr("checked", true);
+                                }
+                                if (data.data.is_custom == null) {
+                                    $(".but").animate({left: -40},0);
+                                    $(".hidde").slideDown();
+                                    $(".module").removeClass("block");
+                                    $(".module_li").addClass("block");
+                                }
+                            } else {
+                                console.log('error: -200');
+                            }
 
+                        },
+                        error: function (xhr) {
+                            console.log(xhr);
+                        }
+                    });
                 } else {
                     parent.layer.msg(result.message);
                     return false;
@@ -32,7 +85,7 @@ $(document).ready(function () {
             url: ApiUrl + 'plat_set_template',
             type: 'post',
             dataType: 'json',
-            data: {template_id: templateId},
+            data: {template_id: templateId, is_custom: 2},
             success: function (data) {
                 if (data.code === 200) {
                     swal('', '保存成功', 'success');
@@ -45,4 +98,6 @@ $(document).ready(function () {
             }
         });
     });
+
+
 });
--- admin/js/template_button.js
+++ admin/js/template_button.js
@@ -1,56 +1,55 @@
 $(function () {
     if (!show) {
-        var show = 2;
+        var show = 1;
     }
     $(".but").click(() => {
-        if (show == 1) {
+        if (show == 2) {
             $(".but").animate({left: -40}, 600);
             $(".hidde").slideDown();
-            show = 2;
+            $(".but").attr("data-toggle","modal");
+            $(".but").attr("data-target","#myModal");
+            show = 1;
         } else {
             $(".but").animate({left: 0}, 600);
             $(".hidde").slideUp();
             $(".module").addClass("block");
             $(".module_li").removeClass("block");
-            show = 1;
+            $(".but").attr("data-toggle","");
+            $(".but").attr("data-target","");
+            show = 2;
         }
     })
 
     $(".form-top > li").each((i, v) => {
         $(v).click(() => {
-            if (i == 1) {
-                $.ajax({
-                    url: ApiUrl + 'plat_setting',
-                    type: 'get',
-                    dataType: 'json',
-                    success: function (data) {
-                        if (data.code === 200) {
-                            $("#templateId").find("input[name=templateId][value=" + data.data.template_id + "]").attr("checked", true);
-                            if (data.data.is_custom == null) {
-                                $(".but").animate({left: -40},0);
-                                $(".hidde").slideDown();
-                                $(".module").removeClass("block");
-                                $(".module_li").addClass("block");
-                            }
-                        } else {
-                            console.log('error: -200');
-                        }
-
-                    },
-                    error: function (xhr) {
-                        console.log(xhr);
-                    }
-                });
-                // javascript:introJs().start();
-            }
             $(v).addClass("shown").siblings().removeClass("shown");
             $(".form-child > li").eq(i).addClass("block").siblings().removeClass("block");
         })
     })
-    $("#updateSet2").click(() => {
-        $(".module").removeClass("block");
-        $(".module_li").addClass("block");
-    })
 
 
+    $("#updateSet2").click(function () {
+        var is_custom = show;
+        var custom_file = $('#custom_file').val();
+        var custom_name = $('#custom_name').val();
+        var template_id = $("#module_list").find("option:selected").val();
+        $.ajax({
+            url: ApiUrl + 'plat_set_template',
+            type: 'post',
+            dataType: 'json',
+            data: {template_id: template_id,custom_name: custom_name, custom_file: custom_file, is_custom: is_custom},
+            success: function (data) {
+                if (data.code === 200) {
+                    swal('', '保存成功', 'success');
+                    $(".module").removeClass("block");
+                    $(".module_li").addClass("block");
+                } else {
+                    swal('', data.message, 'error');
+                }
+            },
+            error: function (xhr) {
+                console.log(xhr);
+            }
+        });
+    });
 })
