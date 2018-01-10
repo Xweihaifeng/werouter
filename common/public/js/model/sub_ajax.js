891fd96a0c67145ebf3569a8ca6a2fd0fe98719a
xiyue_ljx <18435840249@163.com>
2018-01-10 15:50:32 +0800

    分支p

--- common/public/js/model/sub_ajax.js
+++ common/public/js/model/sub_ajax.js
@@ -25,23 +25,17 @@
 	
 	//查询频道分类详情
     sub_ajax.cate = function(params , call){
-        ajax.get('cms/channels/domain_query/'+params).then((res)=>{
+        ajax.get('scms/channels/domain_query/'+params).then((res)=>{
             call(res);
         });
     }
     //查询频道分类
     sub_ajax.cate_tree = function(params , call){
-        ajax.get('/cms/cate_tree_by_channel' , {params : params}).then((res)=>{
+        ajax.get('scms/cate_tree_by_channel' , {params : params}).then((res)=>{
             call(res);
         });
     }
 
-    // 详情页
-    // sub_ajax.cont_list = function(params ,call){
-    //     ajax.get('/cms/detail',{params : params}).then((res)=>{
-    //         call(res);
-    //     });
-    // }
 
 	window.$sub_ajax = sub_ajax;
 })(window);
