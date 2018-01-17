754f5b35220cd1d23e53ba18f27477f6be5a6efb
Xweihaifeng <Xweihaifeng@163.com>
2018-01-17 19:18:46 +0800

    * monify

--- resource/wetpl/substation/default/web/js/main1.js
+++ resource/wetpl/substation/default/web/js/main1.js
@@ -1,33 +1,38 @@
-$("#content_app").ready(function(){
+$("#content_app").ready(function() {
     setTimeout(function(){
+        var len = $(".fc1").children("li").length;
+        $(".fc2").css({ left: len + 'px' });
         // 浙商风采开始
-        if($(".fc1").position()){
-            var b=setInterval(bb,20);
+        var b;
+        if($(".fc1").position()) {
+            b = setInterval(bb, 20);
         }
-        function bb(){
-            var speed=1;
-            var Lefts=$(".fc1").position().left-speed;
-            var Lefts2=$(".fc2").position().left-speed;
-            if(Lefts2==0 || Lefts<-1230){
-                $(".fc1").css({left:1230,zIndex:1})
-            }else{
-                $(".fc1").css({left:Lefts,zIndex:4})
+
+        function bb() {
+            var speed = 1;
+            var Lefts = $(".fc1").position().left - speed;
+            var Lefts2 = $(".fc2").position().left - speed;
+
+            if(Lefts2 == 0 || Lefts <- 195 * len) {
+                $(".fc1").css({ left: 195 * len, zIndex: 1 })
+            } else {
+                $(".fc1").css({ left:Lefts, zIndex:4 })
             }
-            if(Lefts==0 || Lefts2<-1230){
-                $(".fc2").css({left:1230,zIndex:1})
-                // console.log("fc2")
-            }else{
-                $(".fc2").css({left:Lefts2,zIndex:4})
+
+            if(Lefts == 0 || Lefts2 <- 195 * len) {
+                $(".fc2").css({ left: 195 * len, zIndex: 1 })
+            } else {
+                $(".fc2").css({ left: Lefts2, zIndex: 4 })
             }
         }
-        $(".fc-right").mouseenter(function(){
+
+        $(".fc-right").mouseenter(function() {
             clearInterval(b)
         })
-        $(".fc-right").mouseleave(function(){
-            b=setInterval(bb,20)
+
+        $(".fc-right").mouseleave(function() {
+            b = setInterval(bb, 20);
         })
-        // 浙商风采结束
-    },1000);
 
-
+    }, 1500);
 })
