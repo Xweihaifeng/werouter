5270c3b80f88ca568599c61e910d312b514e0e48
HuangYin <kuyuechaosheng@163.com>
2018-02-01 10:37:18 +0800

    *

--- superadm/js/template/page_setting.js
+++ superadm/js/template/page_setting.js
@@ -3,21 +3,21 @@ $(document).ready(function () {
         $.ajax({
             url: ApiUrl + 'plats/pages/setting',
             success: function (data) {
-                if (data.code === 200){
-                    for(let i=0;i < data.data.length;i++){
-                        if(data.data[i].title === 'pageMall'){
+                if (data.code === 200) {
+                    for (let i = 0; i < data.data.length; i++) {
+                        if (data.data[i].title === 'pageMall') {
                             $("#os1").val('pageMall')
-                            if(data.data[i].status == 1){
+                            if (data.data[i].status == 1) {
                                 $(".shop").removeClass('switch-close').addClass('switch-open')
-                            }else {
+                            } else {
                                 $(".shop").removeClass('switch-open').addClass('switch-close')
                             }
                         }
-                        if(data.data[i].title === 'pageArticle'){
+                        if (data.data[i].title === 'pageArticle') {
                             $("#os2").val('pageArticle')
-                            if(data.data[i].status == 1){
+                            if (data.data[i].status == 1) {
                                 $(".test").removeClass('switch-close').addClass('switch-open')
-                            }else {
+                            } else {
                                 $(".test").removeClass('switch-close').addClass('switch-close')
                             }
                         }
@@ -26,25 +26,26 @@ $(document).ready(function () {
             }
         });
     }
+
     req();
-   $(document).on('click','.online_cert .switch',function () {
-       var that = this;
-       var status = $(that).hasClass('switch-open') ? 2 : 1;
-       var title = $(that).children("div").children("input").val();
-       console.log(that)
-       $.ajax({
-           url:ApiUrl + 'plats/pages/setting_status',
-           type:'get',
-           dataType:'json',
-           data:{"status":status,"title":title},
-           success:function (text) {
-               if($(that).hasClass('switch-open')){
-                   $(that).removeClass('switch-open').addClass('switch-close');
-               }else {
-                   $(that).removeClass('switch-close').addClass('switch-open');
-               }
-           }
-       })
-   })
+    $(document).on('click', '.online_cert .switch', function () {
+        var that = this;
+        var status = $(that).hasClass('switch-open') ? 2 : 1;
+        var title = $(that).children("div").children("input").val();
+        console.log(that)
+        $.ajax({
+            url: ApiUrl + 'plats/pages/setting_status',
+            type: "POST",
+            dataType: 'json',
+            data: {status: status, title: title},
+            success: function (text) {
+                if ($(that).hasClass('switch-open')) {
+                    $(that).removeClass('switch-open').addClass('switch-close');
+                } else {
+                    $(that).removeClass('switch-close').addClass('switch-open');
+                }
+            }
+        })
+    })
 
 });
