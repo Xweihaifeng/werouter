c8e9e2d61bf7cea1f0ef51788bad69e5a8dfbe8d
R20609 <479496771@qq.com>
2018-02-01 10:27:37 +0800

    *

--- superadm/js/template/page_setting.js
+++ superadm/js/template/page_setting.js
@@ -1,18 +1,50 @@
 $(document).ready(function () {
-    $(document).on('click', '.set-status', function () {
-        var title = $(this).data('title');
-        var status = $(this).data('status');
+    function req() {
         $.ajax({
-            url: ApiUrl + 'plats/pages/setting_status',
-            type: 'post',
-            data: {title: title, status: status},
-            dataType: 'json',
+            url: ApiUrl + 'plats/pages/setting',
             success: function (data) {
                 if (data.code === 200){
-                    swal({text: '操作成功',type: 'success', timer: 20000});
-                    location.reload();
+                    for(let i=0;i < data.data.length;i++){
+                        if(data.data[i].title === 'pageMall'){
+                            $("#os1").val('pageMall')
+                            if(data.data[i].status == 1){
+                                $(".shop").removeClass('switch-close').addClass('switch-open')
+                            }else {
+                                $(".shop").removeClass('switch-open').addClass('switch-close')
+                            }
+                        }
+                        if(data.data[i].title === 'pageArticle'){
+                            $("#os2").val('pageArticle')
+                            if(data.data[i].status == 1){
+                                $(".test").removeClass('switch-close').addClass('switch-open')
+                            }else {
+                                $(".test").removeClass('switch-close').addClass('switch-close')
+                            }
+                        }
+                    }
                 }
             }
         });
-    });
+    }
+    req();
+   $(document).on('click','.online_cert .switch',function () {
+       var that = this;
+       var status = $(that).hasClass('switch-open') ? 2 : 1;
+       var title = $(that).children("div").children("input").val();
+       console.log(that)
+       $.ajax({
+           url:ApiUrl + 'plats/pages/setting_status',
+           type:'get',
+           dataType:'json',
+           data:{"status":status,"title":title},
+           success:function (text) {
+               if($(that).hasClass('switch-open')){
+                   $(that).removeClass('switch-open').addClass('switch-close');
+               }else {
+                   $(that).removeClass('switch-close').addClass('switch-open');
+               }
+           }
+       })
+   })
+
 });
