ce8eedea7589a2acbcec5c4c70e63db02c6fbe58
xiyue_ljx <18435840249@163.com>
2018-02-03 12:48:48 +0800

    商城详情页

--- resource/wetpl/vertical/web/activity/js/activity.js
+++ resource/wetpl/vertical/web/activity/js/activity.js
@@ -12,4 +12,32 @@ $(function(){
         $(this).addClass("hot").siblings().removeClass("hot");
     })
 
+    // 光标离开事件
+    $(document).on('focus', '#phone', function() {
+        console.log($('.layui-layer').height());
+        $('.sign_code').css('display', 'block');
+        $('.sign_yz').css('display', 'block');
+        $('.signBox .sign_section').css('height', '354px');
+    });
+    $(document).on('focus', '#username', function() {
+        $('.sign_code').css('display', 'none');
+        $('.sign_yz').css('display', 'none');
+        $('.signBox .sign_section').css('height', '306px');
+    });
+    $(document).on('focus', '#zhiw', function() {
+        $('.sign_code').css('display', 'none');
+        $('.sign_yz').css('display', 'none');
+        $('.signBox .sign_section').css('height', '306px');
+    });
+    $(document).on('focus', '#gongsi', function() {
+        $('.sign_code').css('display', 'none');
+        $('.sign_yz').css('display', 'none');
+        $('.signBox .sign_section').css('height', '306px');
+    });
+    $(document).on('focus', '#gongsi', function() {
+        $('.sign_code').css('display', 'none');
+        $('.sign_yz').css('display', 'none');
+        $('.signBox .sign_section').css('height', '306px');
+    });
+
 })
