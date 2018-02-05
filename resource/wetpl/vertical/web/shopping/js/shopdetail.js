d712c298042f0b17bae3a64cbc459db0ca8296b1
xiyue_ljx <18435840249@163.com>
2018-02-05 19:40:56 +0800

    商城，活动，支付修改

--- resource/wetpl/vertical/web/shopping/js/shopdetail.js
+++ resource/wetpl/vertical/web/shopping/js/shopdetail.js
@@ -2,8 +2,11 @@ $(function(){
     // 滚动事件
     var Height=$(".pro_deta_tab").offset().top;
     $(window).scroll(function(){
-        if($(document).scrollTop()>=Height-150){
-            $(".pro_deta_tab").css({position:"fixed",top:56,left:"50%",zIndex:99,marginTop:0,marginLeft:"-585px"});
+
+        if($(document).scrollTop()>=Height-250){
+            $("#userinfo").css({display:"none"});
+            $("#menu").css({position:"relative"})
+            $(".pro_deta_tab").css({position:"fixed",top:0,left:"50%",zIndex:99,marginTop:0,marginLeft:"-585px"});
         }else{
             $(".pro_deta_tab").css({position:"relative",top:0,left:-15,zIndex:1,marginTop:"30px",marginLeft:0});
         }
