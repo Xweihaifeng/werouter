2870d4e2242d25107b66117404584557b419e645
xiyue_ljx <18435840249@163.com>
2018-02-06 19:45:35 +0800

    商城

--- resource/wetpl/vertical/web/shopping/js/shopdetail.js
+++ resource/wetpl/vertical/web/shopping/js/shopdetail.js
@@ -4,12 +4,12 @@ $(function(){
     $(window).scroll(function(){
 
         if($(document).scrollTop()>=Height-250){
-            $("#userinfo").css({display:"none"});
+            // $("#userinfo").css({display:"none"});
             // $("#menu").css({position:"relative"})
-            $(".pro_deta_tab").css({position:"fixed",top:0,left:"50%",zIndex:99,marginTop:0,marginLeft:"-585px"});
+            $(".pro_deta_tab").css({width:820,position:"fixed",top:0,left:"50%",zIndex:99,marginTop:0,marginLeft:"-585px"});
         }else{
             // $("#menu").css({position:"relative"})
-            $(".pro_deta_tab").css({position:"relative",top:0,left:-15,zIndex:1,marginTop:"30px",marginLeft:0});
+            $(".pro_deta_tab").css({position:"relative",top:0,left:0,zIndex:1,marginTop:"30px",marginLeft:0,width:"100%"});
         }
     })
 
