f2870f30f5a05df58db23daf2a56586d3e2a91a3
xiyue_ljx <18435840249@163.com>
2018-02-07 12:20:05 +0800

    商城，活动

--- resource/wetpl/vertical/web/shopping/js/shopdetail.js
+++ resource/wetpl/vertical/web/shopping/js/shopdetail.js
@@ -3,12 +3,13 @@ $(function(){
     var Height=$(".pro_deta_tab").offset().top;
     $(window).scroll(function(){
 
-        if($(document).scrollTop()>=Height-250){
-            // $("#userinfo").css({display:"none"});
-            // $("#menu").css({position:"relative"})
+        if($(document).scrollTop()>=Height-20){
+            console.log(111111)
             $(".pro_deta_tab").css({width:820,position:"fixed",top:0,left:"50%",zIndex:99,marginTop:0,marginLeft:"-585px"});
+            $(".pro_det").css({marginTop:68})
         }else{
             // $("#menu").css({position:"relative"})
+            $(".pro_det").css({marginTop:0})
             $(".pro_deta_tab").css({position:"relative",top:0,left:0,zIndex:1,marginTop:"30px",marginLeft:0,width:"100%"});
         }
     })
